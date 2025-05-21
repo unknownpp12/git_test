e391dcaa61f290677142c8c0dbd07245ff2816cc
Aman <amanpokharel51@gmail.com>
2025-05-21 08:44:26 -0700

    edit javascript.html and javascript.js

---
+++ javascript.js
@@ -0,0 +1,10 @@
+let a = 10;
+console.log(a);
+a = 11;
+console.log(a);
+let b = 7*a;
+console.log(b);
+const max =57;
+const actual =max -13;
+const percentage = actual/max;
+console.log(percentage);
